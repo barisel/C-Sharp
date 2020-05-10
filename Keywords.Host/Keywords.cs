0e5c4fcf0c8a106d82f4990d3fcced0fcabe827b
barisel <bariselvanoglu@gmail.com>
2020-05-10 19:50:28 +0300

    Keywords

---
+++ Keywords.Host/Keywords.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Keywords.Host
+{
+    public class Keywords {
+        #region in : o method içerisinde readonly olur. 
+        static void Method(int argument){
+            argument = 5;
+        }
+
+        // İn 
+        static void Method(in int argument) {
+            //argument = 5; // patlar.
+        }
+
+        public static void Run() {
+            int i = 42;
+            Method(i);
+            Method(in i);
+            i = 5;
+        }
+
+        #endregion
+
+
+    }
+}
